c8caa7af56ea6a3d82b554fddc843334c339cbd6
Keith M <keithgmitchell@g.ucla.edu>
2019-09-29 19:27:05 -0700

    App updates, multiple tabs, marker sets, md tab

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ genes = experiment.merged@assays$RNA
 meta_nums <- colnames(dplyr::select_if(experiment.merged@meta.data, is.numeric))
 meta_cats <- colnames(dplyr::select_if(experiment.merged@meta.data, is.factor))
 pcs <- list('PC_1','PC_2','PC_3','PC_4','PC_5','PC_6','PC_7','PC_8','PC_9')
+agg_cats <- colnames(dplyr::select_if(experiment.merged@meta.data, is.factor))
 
 
 server = function(input, output, session){
@@ -22,6 +23,10 @@ server = function(input, output, session){
   )})
   observe({
     updateSelectInput(session, "numeric2",
+                      choices = outVar()
+  )})
+  observe({
+    updateSelectInput(session, "numeric_b",
                       choices = outVar()
   )})
   formulaText <- reactive({
@@ -51,8 +56,7 @@ server = function(input, output, session){
   output$MarkerGenePlot <- renderPlot({
     FeaturePlot(
       experiment.merged,
-      input$numeric,
-      cols = c("lightgrey", "blue")
+      c("Xkr4", "Tcea1"), blend=TRUE
     )
   })
 
@@ -64,37 +68,94 @@ server = function(input, output, session){
     Idents(experiment.merged) <- input$categorical
     VlnPlot(object =  experiment.merged, features = c(input$numeric, input$numeric2), pt.size = 0.05)
   })
+  
+  output$MarkerSet <- renderPlot({
+    Idents(experiment.merged) <- input$categorical_b
+    markers = input$numeric_b
+    expr.cutoff = 3
+    widedat <- FetchData(experiment.merged, markers)
+    widedat$Cluster <- Idents(experiment.merged)
+    longdat <- gather(widedat, key = "Gene", value = "Expression", -Cluster)
+    longdat$Is.Expressed <- ifelse(longdat$Expression > expr.cutoff, 1, 0)
+    longdat$Cluster <- factor(longdat$Cluster)
+    longdat$Gene <- factor(longdat$Gene)
+    
+    # Need to summarize into average expression, pct expressed (which is also an average)
+    plotdat <- group_by(longdat, Gene, Cluster) %>% summarize(`Percentage of Expressed Cells` = mean(Is.Expressed), `Mean Expression` = mean(Expression))
+    
+    
+    ggplot(plotdat, aes(x = Gene, y = Cluster)) +
+      geom_point(aes(size = `Percentage of Expressed Cells`, col = `Mean Expression`)) +
+      labs(size = "Percentage\nof Expressed\nCells", col = "Mean\nExpression", x = NULL) +
+      scale_color_gradient(low = "grey", high = "slateblue4") + theme_grey(base_size = 15) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  }, height = 1000, width = 900 )
+
+  output$summary <- renderPrint({
+    summary(d())
+  })
+  
+  # Generate an HTML table view of the data ----
+  output$table <- renderTable({
+    d()
+  })
+  
 }
 
 ui <- fluidPage(
-
-  # App title
-  titlePanel("Identifying Marker Genes"),
-
+  
+  # App title ----
+  titlePanel("scRNA Seurat Analysis"),
+  
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
+    
     # Sidebar panel for inputs ----
     sidebarPanel(
-      selectInput("dataset", "Numeric Analysis Type:",
-                  c('Genes', 'Numeric Metadata','PCs')),
-      selectInput("categorical", "Identity:",
-                  c(meta_cats)),
-      selectInput("numeric", "Primary Numeric:", ""),
-
-      selectInput('numeric2', 'Secondary Numeric', "")
-      
+        conditionalPanel(condition = "input.tabselected == -999",
+                selectInput("dataset", "Numeric Analysis Type:",
+                            c('Genes', 'Numeric Metadata','PCs')),
+                selectInput("categorical", "Identity:",
+                            c(meta_cats)),
+                selectInput("numeric", "Primary Numeric:", ""),
+                
+                selectInput('numeric2', 'Secondary Numeric', "")
+        ),
+        
+        conditionalPanel(condition = "input.tabselected == 2",
+                         selectInput("categorical_b", "Identity:",
+                                     c(agg_cats)),
+                         selectInput("numeric_b", "Primary Numeric:", "", multiple=TRUE)
+         ),
+        conditionalPanel(condition = "input.tabselected == 3")
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      #h3(textOutput("caption")),
-      plotOutput("MarkerGenePlot"),
-      #h3(textOutput("caption2")),
-      plotOutput("CategoricalPlot"),
-      #h3(textOutput("caption3")),
-      plotOutput("ViolinPlot")
+      
+      # Output: Tabset w/ plot, summary, and table ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Marker Genes (TSNE)", value=-999,
+                               #h3(textOutput("caption")),
+                               plotOutput("MarkerGenePlot"),
+                               plotOutput("ViolinPlot"),
+                               #h3(textOutput("caption2")),
+                               plotOutput("CategoricalPlot")
+                               #h3(textOutput("caption3")),
+                           ),
+                  
+                  tabPanel("Marker Set (Grid)", value=2,
+                               plotOutput("MarkerSet")
+                          ),
+
+                  tabPanel("Documentation", value=3,
+                           includeMarkdown("docs/testing.md"),
+                           includeMarkdown("docs/todo.md")
+                         ),
+                  
+                  id = "tabselected"
+      )
     )
   )
 )
-
 shinyApp(ui, server)
---
+++ docs/todo.md
@@ -0,0 +1,18 @@
+# Todo:
+- readme documentation tab with the primers/library used
+
+# Done or not needed
+- ensembl gene id's symbol if available 
+- add an option for the PC graphs 
+- marker set tab with the figure Blythe/Matt had seen
+- readme tab (MD)
+- make the side by side (secondary gene) optional
+    - going along with this Matt wanted to change the layout.. I did put the violin plot in the middle
+    - not sure i can group them around the side bar layout as wanted. 
+    
+
+# Not sure how to do it
+- make the default NA
+
+# Questions
+- is it ok to use experiment merged for everything (has more groupings)
